68a84bcc063a17dcb32a8b2f91a6b7f33f21564a
stephaniepazos89 <stephaniepazos89@gmail.com>
2020-10-09 01:45:46 -0300

    Se modela las solicitud de alimentos entre el fornt y el back, de forma asincronica

--- foodoverflow-angular/src/domain/alimento.ts
+++ foodoverflow-angular/src/domain/alimento.ts
@@ -1,14 +1,20 @@
-import { CondicionAlimenticia } from './condicionAlimenticia'
-import { GrupoAlimenticio } from './grupoAlimenticio'
+import { CondicionAlimenticia } from './condicionAlimenticia';
+import { GrupoAlimenticio } from './grupoAlimenticio';
 
 export class Alimento {
+  private listaInadecuado: CondicionAlimenticia[] = [];
 
-   private listaInadecuado: CondicionAlimenticia[] = []
+  constructor(
+    public nombreDeAlimento?: string,//? puede ser no asignada
+    public grupoAlimenticio?: GrupoAlimenticio
+  ) {}
 
-    constructor(public nombreDeAlimento: string, public grupoAlimenticio: GrupoAlimenticio){}
+   static fromJson(alimentoJSON): Alimento {
+    return Object.assign(new Alimento(), alimentoJSON, {});
+  }
 
-    inadecuadoPara = (): CondicionAlimenticia[] => this.listaInadecuado
+  inadecuadoPara = (): CondicionAlimenticia[] => this.listaInadecuado;
 
-    agregarInadecuado = (condicion: CondicionAlimenticia) => this.listaInadecuado.push(condicion)
-
+  agregarInadecuado = (condicion: CondicionAlimenticia) =>
+    this.listaInadecuado.push(condicion);
 }
